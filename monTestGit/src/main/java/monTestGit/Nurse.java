e70e725987a552ab05260ec883ae36c9eaaba84a
adoumbouya53 <adoumbouya53@yahoo.com>
2024-07-23 21:03:19 +0200

    Still a sentence

--- monTestGit/src/main/java/monTestGit/Nurse.java
+++ monTestGit/src/main/java/monTestGit/Nurse.java
@@ -4,6 +4,7 @@ public class Nurse {
 
 	public static void main(String[] args) {
 	System.out.println("L'ignorance fait plus mal que taper ou parler");
+	System.out.println("La vie est belle !!!");
 
 	}
 
